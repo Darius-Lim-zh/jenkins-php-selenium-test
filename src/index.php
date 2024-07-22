47c01dd7b21eadf4f1149b29589f5b185b7627c4
Darius-Lim-zh <dariuslimzh@gmail.com>
2024-07-22 18:21:02 +0800

    temp

--- src/index.php
+++ src/index.php
@@ -48,6 +48,7 @@
 	
 	if(isset($_POST['submit']))
 	{
+        echo "Testing to see where this appears";
 		if((isset($_POST['email']) && $_POST['email'] !='') && (isset($_POST['password']) && $_POST['password'] !=''))
 		{
 			$email = trim($_POST['email']);
